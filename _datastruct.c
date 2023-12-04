1bcd4d627ed1f5f0d8b28729440db48c35bb067b
9-max-6 <mutukumaxwel@gmail.com>
2023-12-04 03:25:45 -0800

    feat(data structures): functions to manipulate linked lists.

---
+++ _datastruct.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_list - a function to print all the elements of a
+ * list_t list
+ * @h: pointer to the head of the list.
+ * 
+ * Return: returns number of elements
+ */
+
+size_t __printlist(const list_t *h)
+{
+	unsigned int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (i);
+}
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void __freelist(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
--- main.h
+++ main.h
@@ -2,15 +2,26 @@
 #define MAIN_H
 
 int _strlen(char *str);
-
- 
-
-
-
 char *_strcpy(char *dest, const char *src);
-
 int starts_with(char *str1, char *str2);
-
 char *_strcat(char *str1, char *str2);
+size_t __printlist(const list_t *h);
+void __freelist(list_t *head);
+
+/**
+ * struct _PATH - a structure of the PATH dirs.
+ * @_dir: pointer to pathname
+ * @next: pointer to the next structure.
+ */
+struct _PATH 
+{
+	char *_dir;
+	char struct _PATH *next;
+}
+
+/**
+ * _path - type definiton of _PATH
+ */
+typedef struct _PATH _path;
 
 #endif
