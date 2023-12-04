6458ba34d3a8598473b1b02d20e8460cf3046282
9-max-6 <mutukumaxwel@gmail.com>
2023-12-04 03:36:31 -0800

    New task

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - the main function of the program.
+ * @av: the argument vector.
+ * @ac: the argument count.
+ */
+
+int main(int ac, char *av[])
+{
+	if (ac > 2)
+	{
+		exit()
+	}
+	
+	return (0);
+	
+}
+
