bb9c03e5fcd742d4a934f169865497216ecc2e6b
George Ikigu <ikigu.geo@gmail.com>
2023-12-05 15:40:44 +0300

    fix(logic): fix string concatenation
    This eliminates memory leaks that were presented by using the _strcat functions. _strcat may still have issues for more than one subsequent concatenation.

--- find_exe.c
+++ find_exe.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * find_exe - finds an executable file in $PATH
@@ -13,30 +14,37 @@
 
 char *find_exe(char *path_tokens[], char *command)
 {
-	int size, i, stat_result;
-	char *full_path_to_exe, *cat_path;
-	struct stat *buf;
+	int i;
+	size_t size;
+	char *full_path_to_exe;
+	struct stat buf;
+
+	char slash[2] = "/";
 
 	if (!command)
-		exit(98);
+		exit(98); /* exit appropriately */
+
+	i = 0;
 
 	while (path_tokens[i])
 	{
-		size = _strlen(path_tokens[i] + _strlen(command) + 1);
+		size = _strlen(path_tokens[i]) + _strlen(command) + 2;
 		full_path_to_exe = malloc(size);
 
 		if (!full_path_to_exe)
-			exit(98);
+			return (NULL);
 
-		cat_path = _strcat(path_tokens[i], command);
-		full_path_to_exe = _strcpy(full_path_to_exe, cat_path);
+		_strcpy(full_path_to_exe, path_tokens[i]);
+		_strcpy((full_path_to_exe + _strlen(full_path_to_exe)), slash);
+	    _strcpy((full_path_to_exe + _strlen(full_path_to_exe)), command);
 
-		stat_result = stat(full_path_to_exe, buf);
-
-		if (stat_result == 0)
+		if (stat(full_path_to_exe, &buf) == 0)
 		{
 			return (full_path_to_exe);
 		}
+
+		i++;
+		free(full_path_to_exe);
 	}
 
 	return (NULL);
