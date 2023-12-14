43de0757ddae034860dd2a44979388538609345f
George Ikigu <ikigu.geo@gmail.com>
2023-12-14 15:26:59 +0300

    feat: add ability to handle spaces in command line
    Add ability to ignore all spaces at beginning of command line

--- read_command.c
+++ read_command.c
@@ -26,6 +26,25 @@ void print_env(char *env[])
 }
 
 /**
+ * handle_spaces - handles spaces in input
+ * @str: string to check for spaces
+ *
+ *  Return: new location of pointer
+*/
+
+char *handle_spaces(char *str)
+{
+	if (str[0] == ' ')
+	{
+		while (str[0] == ' ')
+			str += 1;
+
+	}
+
+	return (str);
+}
+
+/**
  * read_teractive_cmd - handles interactive commands
  * @path: an array of path dirs
  * @buffer: buffer to read the command into
@@ -36,15 +55,19 @@ void print_env(char *env[])
 
 char *read_teractive_cmd(char *path[], char *buffer, char *env[])
 {
+	char *cp, *cp_for_spaces;
+
 	ssize_t bytes_read = 0;
 	size_t buffer_size = BUFFER_SIZE;
-	char *cp;
 
 	while (1)
 	{
 		printf("$ ");
 		bytes_read = getline(&buffer, &buffer_size, stdin);
 		cp = buffer;
+		cp_for_spaces = buffer;
+
+		cp_for_spaces = handle_spaces(cp_for_spaces);
 
 		if (bytes_read == -1)
 		{
@@ -52,19 +75,19 @@ char *read_teractive_cmd(char *path[], char *buffer, char *env[])
 			continue; /* This is the EOF condition */
 		}
 
-		if (buffer[0] == '\n')
+		if (cp_for_spaces[0] == '\n')
 			continue; /* hitting enter restarts the loop */
 
-		if (starts_with("exit", buffer) == 0)
+		if (starts_with("exit", cp_for_spaces) == 0)
 			break;
 
-		if (starts_with("env", buffer) == 0)
+		if (starts_with("env", cp_for_spaces) == 0)
 		{
 			print_env(env);
 			continue;
 		}
 
-		parse_command(buffer, path);
+		parse_command(cp_for_spaces, path);
 	}
 
 	return (cp);
